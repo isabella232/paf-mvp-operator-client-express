b507ddddba54a8ff8d841363560131d6a5fdf2ae
d.kovtun <d.kovtun@criteo.com>
2022-02-02 15:33:04 +0200

    Remove dist and add postinstall script

--- dist/operator-client-proxy.d.ts
+++
@@ -1,3 +0,0 @@
-import { Express } from "express";
-import { PublicKeys } from "paf-mvp-core-js/dist/crypto/keys";
-export declare const addOperatorClientProxyEndpoints: (app: Express, protocol: 'https' | 'http', operatorHost: string, sender: string, privateKey: string, allowedOrigins: string[], publicKeys: PublicKeys) => void;
