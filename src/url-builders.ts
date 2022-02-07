1c777081566ad494990b751251b7930b49ed5d10
Olivier Chirouze <o.chirouze@criteo.com>
2022-02-07 22:24:50 +0100

    Add URL builders for 3PC, new ID, identity

--- src/url-builders.ts
+++ src/url-builders.ts
@@ -1,6 +1,6 @@
-import {GetIdsPrefsRequest, PostIdsPrefsRequest, IdsAndPreferences} from "paf-mvp-core-js/dist/model/generated-model";
+import {GetIdsPrefsRequest, PostIdsPrefsRequest, IdsAndPreferences, GetNewIdRequest} from "paf-mvp-core-js/dist/model/generated-model";
 import {UnsignedMessage} from "paf-mvp-core-js/dist/model/model";
-import {GetIdsPrefsRequestSigner, PostIdsPrefsRequestSigner} from "paf-mvp-core-js/dist/crypto/message-signature";
+import {GetIdsPrefsRequestSigner, PostIdsPrefsRequestSigner, GetNewIdRequestSigner} from "paf-mvp-core-js/dist/crypto/message-signature";
 import {PrivateKey, privateKeyFromString} from "paf-mvp-core-js/dist/crypto/keys";
 import {jsonEndpoints, redirectEndpoints} from "paf-mvp-core-js/dist/endpoints";
 import {QSParam} from "paf-mvp-core-js/dist/query-string";
@@ -46,7 +46,7 @@ export abstract class RestAndRedirectURLBuilder<T> extends RestURLBuilder<T> {
 }
 
 export class GetIdsPrefsURLBuilder extends RestAndRedirectURLBuilder<GetIdsPrefsRequest> {
-    private readonly readSigner = new GetIdsPrefsRequestSigner()
+    private readonly signer = new GetIdsPrefsRequestSigner()
 
     constructor(protocol: "https" | "http", operatorHost: string, host: string, privateKey: string) {
         super(protocol, operatorHost, host, privateKey, jsonEndpoints.read, redirectEndpoints.read);
@@ -60,13 +60,13 @@ export class GetIdsPrefsURLBuilder extends RestAndRedirectURLBuilder<GetIdsPrefs
         }
         return {
             ...request,
-            signature: this.readSigner.sign(this.ecdsaKey, request)
+            signature: this.signer.sign(this.ecdsaKey, request)
         };
     }
 }
 
 export class PostIdsPrefsURLBuilder extends RestAndRedirectURLBuilder<PostIdsPrefsRequest> {
-    private readonly writeSigner = new PostIdsPrefsRequestSigner()
+    private readonly signer = new PostIdsPrefsRequestSigner()
 
     constructor(protocol: "https" | "http", operatorHost: string, host: string, privateKey: string) {
         super(protocol, operatorHost, host, privateKey, jsonEndpoints.write, redirectEndpoints.write);
@@ -81,11 +81,51 @@ export class PostIdsPrefsURLBuilder extends RestAndRedirectURLBuilder<PostIdsPre
         }
         return {
             ...request,
-            signature: this.writeSigner.sign(this.ecdsaKey, request)
+            signature: this.signer.sign(this.ecdsaKey, request)
         };
     }
 
     getRestUrl(request: PostIdsPrefsRequest): URL {
         return this.getOperatorUrl(this.restEndpoint) // /!\ not passing any object because it will be sent as POST!
+    }
+}
+
+export class GetNewIdURLBuilder extends RestURLBuilder<GetNewIdRequest> {
+    private readonly signer = new GetNewIdRequestSigner()
+
+    constructor(protocol: "https" | "http", operatorHost: string, host: string, privateKey: string) {
+        super(protocol, operatorHost, host, privateKey, jsonEndpoints.newId);
+    }
+
+    buildRequest(timestamp = new Date().getTime()): GetNewIdRequest {
+        const request: UnsignedMessage<GetNewIdRequest> = {
+            sender: this.host,
+            receiver: this.operatorHost,
+            timestamp
+        }
+        return {
+            ...request,
+            signature: this.signer.sign(this.ecdsaKey, request)
+        };
+    }
+}
+
+export class Get3PCURLBuilder extends RestURLBuilder<undefined> {
+    constructor(protocol: "https" | "http", operatorHost: string, host: string, privateKey: string) {
+        super(protocol, operatorHost, host, privateKey, jsonEndpoints.verify3PC);
+    }
+
+    buildRequest(timestamp = new Date().getTime()): undefined {
+        return undefined;
+    }
+}
+
+export class GetIdentityURLBuilder extends RestURLBuilder<undefined> {
+    constructor(protocol: "https" | "http", operatorHost: string, host: string, privateKey: string) {
+        super(protocol, operatorHost, host, privateKey, jsonEndpoints.identity);
+    }
+
+    buildRequest(timestamp = new Date().getTime()): undefined {
+        return undefined;
     }
 }
